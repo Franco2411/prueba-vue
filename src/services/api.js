802f6def9e53f43a7c6b9559eb873725c2e0d32c
Franco Allovero <fra98nba@gmail.com>
2024-10-16 20:05:39 -0300

    Cambio url api backend

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Aquí puedes configurar la URL base de tu backend
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000', // Cambia esta URL por la de tu backend
+  baseURL: 'https://safe-gorge-77661-fab8bec35bb4.herokuapp.com', // Cambia esta URL por la de tu backend
   headers: {
     'Content-Type': 'application/json',
   },
